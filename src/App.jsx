6b3a93c09af41d4146325e678ccc9968d75ed452
Your Name <you@example.com>
2025-08-06 03:36:53 +0530

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import About from './pages/AboutPage.jsx';
+import Contact from './pages/contact.jsx';
 const App = () => {
   return (
     <Routes>
       {/* <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} /> */}
       <Route path="/" element={<About />} />
+      <Route path="/Contact" element={<Contact/>} />
+
     </Routes>
   )
 }
