e42e041622764780428425be9570caf21aff77cb
Rasul <rasulabaccotech@gmail.com>
2025-08-06 03:29:58 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import About from './pages/AboutPage.jsx';
+import HomePage from './pages/HomePage.jsx';
 const App = () => {
   return (
     <Routes>
       {/* <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} /> */}
-      <Route path="/" element={<About />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   )
 }
