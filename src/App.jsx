402fafa23578591aca89c179feb265d76e24d57f
AshokAbacco <ashokgowd34@gmail.com>
2025-08-06 22:50:47 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,15 @@ import Contact from './pages/Contact.jsx';
 
 import HomePage from './pages/HomePage.jsx';
 import ServicePage from './pages/ServicePage.jsx';
-<<<<<<< HEAD
 import SoftService from './pages/SoftService.jsx';
 import PestControlServices from './pages/PestControlServices.jsx';
 import Contract from './pages/ContractStaffingServices.jsx'
-=======
-
->>>>>>> a62de27f1eb8acdd0503b4eba032965b03985d36
 
 const App = () => {
   return (
     <Routes>
       {/* <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} /> */}
-<<<<<<< HEAD
 
       <Route path="/Contact" element={<Contact/>} />
       <Route path="/" element={<HomePage />} />
@@ -29,14 +24,6 @@ const App = () => {
       <Route path="/sofrservices" element={<SoftService />} />
       <Route path="/pestcontrol" element={<PestControlServices />} />
       <Route path="/contract" element={<Contract />} />
-=======
-      
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="/services" element={<ServicePage />} />
-      
->>>>>>> a62de27f1eb8acdd0503b4eba032965b03985d36
 
     </Routes>
   )
