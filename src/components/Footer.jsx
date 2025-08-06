43c44a3a1a7b1349e4b5b58e878f69d2572d49f7
Your Name <you@example.com>
2025-08-07 00:52:33 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,17 +27,13 @@ const Footer = () => {
         <div>
           <h3 className="text-lg font-semibold mb-3 text-white">About</h3>
           <ul className="space-y-2 text-gray-300">
-<<<<<<< HEAD
+
             <li><Link to="#">Company</Link></li>
             <li><Link to="#">FAQs</Link></li>
             <li><Link to="#">News</Link></li>
             <li><Link to="#">Recent Updates</Link></li>
-=======
-            <li><a href="#">Company</a></li>
-            <li><Link to="/FAQ">FAQ's</Link></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Recent Updates</a></li>
->>>>>>> main
+
+          
           </ul>
         </div>
 
@@ -55,17 +51,12 @@ const Footer = () => {
         <div>
           <h3 className="text-lg font-semibold mb-3 text-white">Support</h3>
           <ul className="space-y-2 text-gray-300">
-<<<<<<< HEAD
+
             <li><Link to="/contact">Customer Support</Link></li>
             <li><Link to="#"> Updates</Link></li>
             <li><Link to="#">Terms & Conditions</Link></li>
             <li><Link to="#">Privacy Policy</Link></li>
-=======
-            <li><a href="#">Customer Support</a></li>
-            <li><a href="#"> Updates</a></li>
-            <li><Link to="/terms-and-privacy">Privacy Policy</Link></li>
-            <li><Link to="/terms-and-privacy">Terms & Conditions</Link></li>
->>>>>>> main
+
           </ul>
         </div>
       </div>
