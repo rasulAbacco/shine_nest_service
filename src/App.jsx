1b889869d1b54a2d6db39e5bcdce437d7cd91066
Rasul <rasulabaccotech@gmail.com>
2025-08-06 21:21:41 +0530

    scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,27 @@ import Contact from './pages/Contact.jsx';
 import HomePage from './pages/HomePage.jsx';
 import ServicePage from './pages/ServicePage.jsx';
 import TestimonialsSection from './pages/Testimonials.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 
 
 
 const App = () => {
   return (
-    <Routes>
-      {/* <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} /> */}
-      
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="/services" element={<ServicePage />} />
-      <Route path="/testimonials" element={<TestimonialsSection />} />
+    <>
+      <ScrollToTop />
+      <Routes>
 
-    </Routes>
+        {/* <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} /> */}
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<ServicePage />} />
+        <Route path="/testimonials" element={<TestimonialsSection />} />
+
+      </Routes>
+    </>
   )
 }
 
